2bb33384892d443dc45e74926501d40098e5771a
Taras Nurko <tarasnurko84@gmail.com>
2022-08-09 10:27:15 +0300

    feat: created BreedInfo page

--- src/components/Pages/BreedInfo.jsx
+++ src/components/Pages/BreedInfo.jsx
@@ -16,6 +16,31 @@ const BreedInfo = () => {
           <PageInfo active>28</PageInfo>
         </div>
         <Carousel />
+        <div className={styles.bottom}>
+          <h2 className={styles.name}>Basenji</h2>
+          <div className={styles.block}>
+            <div className={styles.info}>
+              <div className={styles.datum}>Temperament:</div>
+              <div className={styles.text}>
+                Active, Energetic, Independent, Intelligent, Gentle
+              </div>
+            </div>
+          </div>
+          <div className={styles.block}>
+            <div className={styles.info}>
+              <div className={styles.datum}>Origin:</div>
+              <div className={styles.text}>United States</div>
+            </div>
+            <div className={styles.info}>
+              <div className={styles.datum}>Weight:</div>
+              <div className={styles.text}>3 - 5 kgs</div>
+            </div>
+            <div className={styles.info}>
+              <div className={styles.datum}>Life span:</div>
+              <div className={styles.text}>14 - 15 years</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
