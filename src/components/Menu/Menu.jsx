76359f191dd1fa3b5bc18c52d7b438798c7714fe
Taras Nurko <tarasnurko84@gmail.com>
2022-08-10 13:26:53 +0300

    feat: added active styling to Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Menu.module.scss";
 
 import { VotingImage, BreedsImage, GalleryImage } from "../../assets/index";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Menu = () => {
+  const url = useLocation();
+
+  useState(() => {}, []);
+
   return (
     <div className={styles.menu}>
-      <Link to="/voting" className={styles.item}>
+      <Link
+        to="/voting"
+        className={
+          url.pathname === "/voting"
+            ? `${styles.item} ${styles.active}`
+            : `${styles.item} ${styles.default}`
+        }
+      >
         <div className={`${styles.wrapper} ${styles["wrapper-voting"]}`}>
           <img src={VotingImage} alt="voting" />
         </div>
         <button className={styles.button}>VOTING</button>
       </Link>
 
-      <Link to="/breeds" className={styles.item}>
+      <Link
+        to="/breeds"
+        className={
+          url.pathname === "/breeds"
+            ? `${styles.item} ${styles.active}`
+            : `${styles.item} ${styles.default}`
+        }
+      >
         <div className={`${styles.wrapper} ${styles["wrapper-breeds"]}`}>
           <img src={BreedsImage} alt="breeds" />
         </div>
         <button className={styles.button}>BREEDS</button>
       </Link>
 
-      <Link to="/gallery" className={styles.item}>
+      <Link
+        to="/gallery"
+        className={
+          url.pathname === "/gallery"
+            ? `${styles.item} ${styles.active}`
+            : `${styles.item} ${styles.default}`
+        }
+      >
         <div className={`${styles.wrapper} ${styles["wrapper-gallery"]}`}>
           <img src={GalleryImage} alt="gallery" />
         </div>
